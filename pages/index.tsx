89ed23a2e461820a80a61b8745d7179324587abe
UchidaSoichiro <oribeat@ezweb.ne.jp>
2024-08-01 03:14:03 +0900

    下準備

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
-
+import { NextPage } from "next";
+ 
+const IndexPage: NextPage = () => {
+  return <div>猫画像予定地</div>;
+};
 export default IndexPage;
