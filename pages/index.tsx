d22dd517afcbc1f0faae595c3b86e813d163f8ea
UchidaSoichiro <oribeat@ezweb.ne.jp>
2024-08-01 03:35:21 +0900

    あ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
-import { NextPage } from "next";
- 
-const IndexPage: NextPage = () => {
-  return <div>猫画像予定地</div>;
+import React from 'react';
+import Tabs from '../components/Tabs';
+
+const Home: React.FC = () => {
+  const tabs = [
+    { title: 'Tab 1', content: <div>Content for Tab 1</div> },
+    { title: 'Tab 2', content: <div>Content for Tab 2</div> },
+    { title: 'Tab 3', content: <div>Content for Tab 3</div> }
+  ];
+
+  return (
+    <div>
+      <h1>My Tabs</h1>
+      <Tabs tabs={tabs} />
+    </div>
+  );
 };
-export default IndexPage;
+
+export default Home;
